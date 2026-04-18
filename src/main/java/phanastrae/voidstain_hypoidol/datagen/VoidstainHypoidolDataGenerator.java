93332daff8ef7f6565c790a66bb58e6691f9f1f5
Phanastrae <111198528+Phanastrae@users.noreply.github.com>
2026-04-18 13:34:28 +0100

    Cleanup unwanted template things

--- src/main/java/phanastrae/voidstain_hypoidol/datagen/VoidstainHypoidolDataGenerator.java
+++ src/main/java/phanastrae/voidstain_hypoidol/datagen/VoidstainHypoidolDataGenerator.java
@@ -1,4 +1,4 @@
-package phanastrae.voidstain_hypoidol;
+package phanastrae.voidstain_hypoidol.datagen;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
