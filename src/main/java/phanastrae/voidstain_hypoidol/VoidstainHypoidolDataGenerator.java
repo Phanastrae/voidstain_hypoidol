1739da687c310b9bf6095abf257402762905f565
Phanastrae <111198528+Phanastrae@users.noreply.github.com>
2026-04-18 12:57:17 +0100

    Add fabric template files

---
+++ src/main/java/phanastrae/voidstain_hypoidol/VoidstainHypoidolDataGenerator.java
@@ -0,0 +1,11 @@
+package phanastrae.voidstain_hypoidol;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class VoidstainHypoidolDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
